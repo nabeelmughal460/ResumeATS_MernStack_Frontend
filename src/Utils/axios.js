79778a82362a6909c45d45e6f7c2a967381016e3
Nabeelmughal460 <nabeelmughal460@gmail.com>
2026-01-07 03:56:46 +0500

    axious chnges

--- src/Utils/axios.js
+++ src/Utils/axios.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
+
+// This logic automatically switches between production and local
+const baseURL = import.meta.env.PROD 
+  ? 'https://resumeatsmernstackbackend-production.up.railway.app' 
+  : 'http://localhost:8080';
+
 const instance = axios.create({
-  baseURL: '/', // Replace with your backend URL
-  // timeout: 5000,
-  // headers: {
-  //   'Content-Type': 'application/json',
-  // },
+  baseURL: baseURL,
+  withCredentials: true, // Required for cookies/sessions
 });
+
 export default instance;
